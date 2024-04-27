ee50977c43e637e5848a0362eff6c318eef95419
1754riya <167575288+1754riya@users.noreply.github.com>
2024-04-27 20:38:50 +0530

    Add files via upload

---
+++ tic tac toe.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+
+void displayBoard() {
+    
+		cout<<"\t\tTIC TAC TOE GAME"<<endl;
+	cout<<"Payer1[X]"<<endl;
+	cout<<"Player2[O]"<<endl;
+	cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[0][0]<<"  | "<<board[0][1]<<"   |  "<<board[0][2]<<" \n";
+    cout<<"\t\t_____|_____|_____\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[1][0]<<"  | "<<board[1][1]<<"   |  "<<board[1][2]<<" \n";
+    cout<<"\t\t_____|_____|_____\n";
+    cout<<"\t\t     |     |     \n";
+    cout<<"\t\t  "<<board[2][0]<<"  | "<<board[2][1]<<"   |  "<<board[2][2]<<" \n";
+    cout<<"\t\t     |     |     \n";
+}
+
+int main() {
+    int player, choice;
+    cin>>player;
+
+    char mark;
+    bool gameover = false;
+
+    do {
+        displayBoard();
+        if(player%2){
+        	player=1;
+		}
+		else{
+			player=2;
+		}
+        cout << "Player " << player << ", enter a number: ";
+        cin >> choice;
+        if(player == 1){
+        	mark ='X';
+		}
+		else{
+			mark ='0';
+		}
+
+        if (choice == 1 && board[0][0] == '1')
+            board[0][0] = mark;
+        else if (choice == 2 && board[0][1] == '2')
+            board[0][1] = mark;
+        else if (choice == 3 && board[0][2] == '3')
+            board[0][2] = mark;
+        else if (choice == 4 && board[1][0] == '4')
+            board[1][0] = mark;
+        else if (choice == 5 && board[1][1] == '5')
+            board[1][1] = mark;
+        else if (choice == 6 && board[1][2] == '6')
+            board[1][2] = mark;
+        else if (choice == 7 && board[2][0] == '7')
+            board[2][0] = mark;
+        else if (choice == 8 && board[2][1] == '8')
+            board[2][1] = mark;
+        else if (choice == 9 && board[2][2] == '9')
+            board[2][2] = mark;
+        else {
+            cout << "Invalid move!";
+            player--;
+        }
+
+        gameover = false;
+        
+    } while (!gameover);
+
+    return 0;
+}
